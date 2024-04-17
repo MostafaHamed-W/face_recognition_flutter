55cc56a024c948e36420ebee534a94dff6b7158f
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-04-17 14:06:38 +0200

    Adding Face Detection Library in Flutter

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,10 +1,8 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'RecognitionScreen.dart';
 import 'RegistrationScreen.dart';
-
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -21,24 +19,37 @@ class _HomePageState extends State<HomeScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(margin: const EdgeInsets.only(top: 100),child: Image.asset("images/logo.png",width: screenWidth-40,height: screenWidth-40,)),
+          Container(
+              margin: const EdgeInsets.only(top: 100),
+              child: Image.asset(
+                "images/logo.png",
+                width: screenWidth - 40,
+                height: screenWidth - 40,
+              )),
           Container(
             margin: const EdgeInsets.only(bottom: 50),
             child: Column(
               children: [
-                ElevatedButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>const RegistrationScreen()));
-                },
-                  style: ElevatedButton.styleFrom(minimumSize: Size(screenWidth-30, 50)), child: const Text("Register"),),
-                Container(height: 20,),
-                ElevatedButton(onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>const RecognitionScreen()));
-                },
-                  style: ElevatedButton.styleFrom(minimumSize: Size(screenWidth-30, 50)), child: const Text("Recognize"),),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const RegistrationScreen()));
+                  },
+                  style: ElevatedButton.styleFrom(minimumSize: Size(screenWidth - 30, 50)),
+                  child: const Text("Register"),
+                ),
+                Container(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const RecognitionScreen()));
+                  },
+                  style: ElevatedButton.styleFrom(minimumSize: Size(screenWidth - 30, 50)),
+                  child: const Text("Recognize"),
+                ),
               ],
             ),
           ),
-
         ],
       ),
     );
