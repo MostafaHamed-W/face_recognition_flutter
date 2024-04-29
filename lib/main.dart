7a5acda6599f4c216583f699f5638bc9b9d11fb6
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-04-29 18:53:46 +0300

    Displaying live camera inside the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,73 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/widgets.dart';
 import 'HomeScreen.dart';
 
-void main() {
+late List<CameraDescription> _cameras;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  _cameras = await availableCameras();
   runApp(
-    const MaterialApp(
-      home: HomeScreen(),
-    ),
-  );
+      // const MaterialApp(
+      //   home: HomeScreen(),
+      // ),
+      const CameraApp());
+}
+
+class CameraApp extends StatefulWidget {
+  const CameraApp({super.key});
+
+  @override
+  State<CameraApp> createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  late CameraController controller;
+  @override
+  void initState() {
+    controller = CameraController(_cameras[0], ResolutionPreset.high);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            // Handle access errors here.
+            break;
+          default:
+            // Handle other errors here.
+            break;
+        }
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return MaterialApp(
+      home: Column(
+        children: [
+          CameraPreview(controller),
+          Expanded(
+            child: Container(
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
